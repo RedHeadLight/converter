eabd1e74f05cc07aae3cc86d99b9b50c33610a99
Svetlana Kozlova <ozzla@mail.ru>
2024-12-11 19:29:46 +0300

    Disabled CSS grouping.

--- gulp/tasks/scss.js
+++ gulp/tasks/scss.js
@@ -16,7 +16,7 @@ export const scss = () => {
     })))
     .pipe(sass({ outputStyle: 'expanded' }))
     .pipe(app.plugins.replace(/@img\//g, '../images/'))
-    .pipe(app.plugins.if(app.isBuild, groupCssMediaQueries()))
+    // .pipe(app.plugins.if(app.isBuild, groupCssMediaQueries()))
     // .pipe(app.plugins.if(app.isBuild, webpcss({
     //   webpClass: '.webp',
     //   noWebpClass: '.no-webp'
